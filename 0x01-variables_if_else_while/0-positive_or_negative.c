d2eec955af1ae1f475166d78c1ca3442b07c820b
Muhire Yvan Sebastien <sebastienmuhire1@outlook.com>
2023-07-14 11:17:34 -0700

    hey there

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,10 +13,16 @@ int main(void)
     n = rand() - RAND_MAX / 2;
     printf("%d is ", n);
     if (n > 0)
+    {
         printf("positive\n");
+    }
     else if (n == 0)
+    {
         printf("zero\n");
+    }
     else
+    {
         printf("negative\n");
+    }
     return (0);
 }
