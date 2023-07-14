2e9cdad76fda72ef2624ba92c1a486f860e22fa1
Muhire Yvan Sebastien <sebastienmuhire1@outlook.com>
2023-07-14 14:56:10 -0700

    alx 0x00. C - Hello, World project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - a c program that use puts function to print
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
---
+++ README.md
@@ -0,0 +1,1 @@
+this is readme file for alx-low_level_programming
