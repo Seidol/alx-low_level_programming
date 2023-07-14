358756ef45b78cff105250caa0db9f7de1b838ed
Muhire Yvan Sebastien <sebastienmuhire1@outlook.com>
2023-07-13 17:10:49 -0700

    alx hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include<unistd.h>
 /**
  * main -Entry point
- * Return always 1(succes)
+ * Return always 1 (succes)
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (success)
+ * main - a c program that use puts function to print
+ * Return: Always 0 (Success)
  */
-int mai(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
