29beee4f294f9cc6ef9201918d96b255ce39ab39
Muhire Yvan Sebastien <sebastienmuhire1@outlook.com>
2023-07-20 15:16:52 -0700

    alx more functions nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two int
+ * @a: int must be multiplied to b
+ * @b: int must be multiplied to a
+ * Return: the result of the operation
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
