41d2d28073fc86395f8c8ae76ab818497cf82e08
Muhire Yvan Sebastien <sebastienmuhire1@outlook.com>
2023-08-08 11:42:59 -0700

    this an arry of size with char c  that unsigned int type  and char type  that Return pointer to array created

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - create an arr of size with each element initialized to char c
+ * @size: unsigned int type
+ * @c: char type
+ * Return: Return pointer to the created array
+ */
+
+char *create_array(unsigned int size, char c);
+{
+	char *a;
+	unsigned int b;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	s = malloc((size) * sizeof(char));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	b = 0;
+	while (b < size)
+	{
+		s[b] = c;
+		b++;
+	}
+	s[b] = '\0';
+	return (s);
+}
