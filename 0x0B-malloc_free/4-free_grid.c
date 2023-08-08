f504b447e0271c1c7d022f566ad58efb4ac7140d
Muhire Yvan Sebastien <sebastienmuhire1@outlook.com>
2023-08-08 13:51:23 -0700

    alx free and malloc function program i used do while loop to them

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - Frees memory of a 2D array
+ * @grid: Pointer to the 2D grid
+ * @height: Height dimension of the grid
+ *
+ * Description: Frees the memory of the 2D grid
+ * Return: Nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	do {
+		free(grid[i]);
+		i++;
+	} while (i < height);
+
+	free(grid);
+}
