d69391912a3bd2bd504b249b7b1bf2a058c31145
Seidol <sebastienmuhire1@outlook.com>
2023-08-14 23:50:12 +0200

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This will display the name of the file
+ *
+ * Return: return 0 always successful
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
