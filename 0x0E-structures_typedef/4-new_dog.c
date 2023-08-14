0dc7a7e27b9663ad50a2f68e8d6f7817bb82d359
Muhire Yvan Sebastien <sebastienmuhire1@outlook.com>
2023-08-14 16:45:05 -0700

    0x0E-structures_typedef#

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,109 +1,85 @@
-#include <stdlib.h>
 #include "dog.h"
 
-int custom_strlen(char *s);
-char *custom_strcpy(char *dest, char *src);
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
 
 /**
-  * new_dog - Creates a new dog structure.
-  * @name: The name of the dog.
-  * @age: The age of the dog.
-  * @owner: The owner of the dog.
-  *
-  * Return: A pointer to the new dog structure.
-  */
+ * _strcpy - Copies a source string to a destination buffer.
+ * @dest: The destination buffer.
+ * @src: The source string.
+ *
+ * Return: A pointer to the destination buffer.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+	int i;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - Creates a new dog structure.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: A pointer to the new dog structure (Success), NULL otherwise.
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *puppy_dog;
-	int name_length = 0, owner_length = 0;
+	dog_t *dog;
+	int len1, len2;
 
-	if (name != NULL && owner != NULL)
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
 	{
-		name_length = custom_strlen(name) + 1;
-		owner_length = custom_strlen(owner) + 1;
-		puppy_dog = malloc(sizeof(dog_t));
-
-		if (puppy_dog == NULL)
-			return (NULL);
-
-		puppy_dog->name = malloc(sizeof(char) * name_length);
-
-		if (puppy_dog->name == NULL)
-		{
-			free(puppy_dog);
-			return (NULL);
-		}
-
-		puppy_dog->owner = malloc(sizeof(char) * owner_length);
-
-		if (puppy_dog->owner == NULL)
-		{
-			free(puppy_dog->name);
-			free(puppy_dog);
-			return (NULL);
-		}
-
-		char *name_copy = puppy_dog->name;
-		char *owner_copy = puppy_dog->owner;
-		char *name_src = name;
-		char *owner_src = owner;
-
-		while (*name_src)
-		{
-			*name_copy++ = *name_src++;
-		}
-		*name_copy = '\0';
-
-		while (*owner_src)
-		{
-			*owner_copy++ = *owner_src++;
-		}
-		*owner_copy = '\0';
-
-		puppy_dog->age = age;
+		free(dog);
+		return (NULL);
 	}
-
-	return (puppy_dog);
-}
-
-/**
-  * custom_strlen - Returns the length of a string.
-  * @s: The string to count.
-  *
-  * Return: The length of the string.
-  */
-int custom_strlen(char *s)
-{
-	int length = 0;
-
-	while (*s != '\0')
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
 	{
-		length++;
-		s++;
+		free(dog);
+		free(dog->name);
+		return (NULL);
 	}
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
 
-	return (length);
-}
-
-/**
-  * custom_strcpy - Copies a string.
-  * @dest: The destination buffer.
-  * @src: The source string.
-  *
-  * Return: A pointer to the destination buffer.
-  */
-char *custom_strcpy(char *dest, char *src)
-{
-	char *original_dest = dest;
-
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-
-	*dest = '\0';
-
-	return (original_dest);
+	return (dog);
 }
