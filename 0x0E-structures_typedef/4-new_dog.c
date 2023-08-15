8d0ab26e0f224a66569ba3a90d7cefd195178054
Muhire Yvan Sebastien <sebastienmuhire1@outlook.com>
2023-08-14 17:14:45 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,48 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "dog.h"
-
-/**
- * _strlen - Calculates the length of a string.
- * @s: The string to be measured.
- *
- * Return: The length of the string.
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-
-	return (i);
-}
-
-/**
- * _strcpy - Copies a source string to a destination buffer.
- * @dest: The destination buffer.
- * @src: The source string.
- *
- * Return: A pointer to the destination buffer.
- */
-char *_strcpy(char *dest, char *src)
-{
-	int len = 0;
-	int i;
-
-	while (src[len] != '\0')
-	{
-		len++;
-	}
-
-	for (i = 0; i < len; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-
-	return (dest);
-}
 
 /**
  * new_dog - Creates a new dog structure.
@@ -54,32 +13,32 @@ char *_strcpy(char *dest, char *src)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
-	int len1, len2;
+	dog_t *new_dog_ptr;
 
-	len1 = _strlen(name);
-	len2 = _strlen(owner);
-
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-		return (NULL);
-
-	dog->name = malloc(sizeof(char) * (len1 + 1));
-	if (dog->name == NULL)
+	new_dog_ptr = malloc(sizeof(dog_t));
+	if (new_dog_ptr == NULL)
 	{
-		free(dog);
 		return (NULL);
 	}
-	dog->owner = malloc(sizeof(char) * (len2 + 1));
-	if (dog->owner == NULL)
+
+	new_dog_ptr->name = malloc(strlen(name) + 1);
+	if (new_dog_ptr->name == NULL)
 	{
-		free(dog);
-		free(dog->name);
+		free(new_dog_ptr);
 		return (NULL);
 	}
-	_strcpy(dog->name, name);
-	_strcpy(dog->owner, owner);
-	dog->age = age;
+	strcpy(new_dog_ptr->name, name);
 
-	return (dog);
+	new_dog_ptr->owner = malloc(strlen(owner) + 1);
+	if (new_dog_ptr->owner == NULL)
+	{
+		free(new_dog_ptr->name);
+		free(new_dog_ptr);
+		return (NULL);
+	}
+	strcpy(new_dog_ptr->owner, owner);
+
+	new_dog_ptr->age = age;
+
+	return (new_dog_ptr);
 }
