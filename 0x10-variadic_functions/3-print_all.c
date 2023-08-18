768315bbf6f3adc105d3d1c14e91be814ff6d264
Muhire Yvan Sebastien <sebastienmuhire1@outlook.com>
2023-08-18 12:23:24 -0700

    Write a function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -18,29 +18,26 @@ void print_all(const char * const format, ...)
 	{
 		for (i = 0; format[i]; i++)
 		{
-			if (format[i] == 'c')
+			switch (format[i])
 			{
-				printf("%s%c", s, va_arg(list, int));
-			}
-			else if (format[i] == 'i')
-			{
-				printf("%s%d", s, va_arg(list, int));
-			}
-			else if (format[i] == 'f')
-			{
-				printf("%s%f", s, va_arg(list, double));
-			}
-			else if (format[i] == 's')
-			{
-				st = va_arg(list, char *);
-				if (!st)
-					st = "(nil)";
-				printf("%s%s", s, st);
-			}
-			else
-			{
-				i++;
-				continue;
+				case 'c':
+					printf("%s%c", s, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", s, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", s, va_arg(list, double));
+					break;
+				case 's':
+					st = va_arg(list, char *);
+					if (!st)
+						st = "(nil)";
+					printf("%s%s", s, st);
+					break;
+				default:
+					i++;
+					continue;
 			}
 
 			s = ", ";
@@ -50,4 +47,3 @@ void print_all(const char * const format, ...)
 	printf("\n");
 	va_end(list);
 }
-
