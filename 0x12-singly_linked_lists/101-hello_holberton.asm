73deae16b5c031a76c00e0937e56b1405e8175bf
Muhire Yvan Sebastien <sebastienmuhire1@outlook.com>
2023-08-24 16:20:19 -0700

    64-bit program in assembly that prints Hello, Holberton, followed by a new line.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,9 @@
-section .data
-    format db `Hello, Holberton\n`, 0
-
-section .text
-    global _start
-
-_start:
-    mov   edi, format
-    xor   eax, eax
-    call  printf
-
-    mov   eax, 1
-    xor   edi, edi
-    syscall
-
-section .bss
-    resb 1
+	global   main
+	  extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
