01797f0fdf09a132f7c9102563085469e183630f
Seidol <sebastienmuhire1@outlook.com>
2023-08-29 22:22:12 +0200

    a function that frees a listint_t list.

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - will free a linked list
+ * @h: pointer to the first node.
+ *
+ * Return: all elements in the list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t len = 0;
+	listint_t *temp;
+
+	if (!h || !*h)
+		return (0);
+
+	for (; *h; len++)
+	{
+		temp = (*h)->next;
+		(*h)->next = NULL;
+		free(*h);
+		*h = temp;
+
+		if ((uintptr_t)*h <= (uintptr_t)temp)
+		{
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (len);
+}
